9dbb0e387cc16ca29460a8f8e3d76e5e69ca4fde
GalinaLegchilova <100135406+GalinaLegchilova@users.noreply.github.com>
2022-05-20 17:12:38 +0300

    feat: add exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,20 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 process.stdin.resume();
-const readline = require('readline');
-const { stdin: input, stdout: output, exit } = require('process');
-
-const rl = readline.createInterface({ input, output });
-
-rl.question('Hi! What do you think of Node.js?', (answer) => {
-    });
-rl.on("SIGINT", function () {
-    console.log('Thank you, buy');
-    process.emit("SIGINT");
-    process.exit();
-  });
+process.stdin.setEncoding('utf-8');
+const { stdin, stdout} = process;
+console.log('Hi! What do you think of Node.js?');
 
 const outputTxt = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
-process.stdin.on('data', chunk => outputTxt.write(chunk));
+process.stdin.on('data', chunk => { 
+        const str = chunk.toString(); 
+    if (str.toLowerCase().trim() === 'exit') {
+        stdout.write('Thank you, buy \n');
+        process.exit(); 
+    }
+    else{
+        outputTxt.write(chunk)
+    }
+});
 process.stdin.on('error', error => console.log('Error', error.message));
+process.on('SIGINT', () => {
+    stdout.write('Thank you, buy \n');
+    process.exit();
+  });
