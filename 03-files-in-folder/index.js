372f13796d16e572814a6db4c433cd0b8b6add44
GalinaLegchilova <100135406+GalinaLegchilova@users.noreply.github.com>
2022-05-25 13:59:20 +0300

    feat: change sync

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,10 +1,17 @@
-const fs = require('fs');
-const path = require('path')
-fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
-    if(err) throw err; 
-    for(let i = 0; i < files.length; i++){
-		console.log(files[i].split(".")[0] + ' - ' + files[i].split(".")[1]+ ' - ' +
-        fs.statSync(path.join(__dirname, 'secret-folder/') + files[i]).size/1000 + 'kb'
-        )
-	}
-});
+const fs = require("fs");
+const path = require("path");
+
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, files) =>  {
+  if(err) throw err; 
+  else {
+       files.forEach((file) => {
+        const filePath = path.join(__dirname, 'secret-folder/', file.name);
+        fs.stat(path.join(__dirname, 'secret-folder/', file.name), (error, stats) => {
+          if (stats.isFile()) {
+            console.log(`${file.name.split(".")[0]} - ${file.name.split(".")[1]} - ${stats.size/1000} kb`);
+          }
+        });
+      });
+    }
+  }
+);
